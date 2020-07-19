b2c5897b3fa6d18b6b7e781df836fca49b1eb9a6
Mark Kirshner <zhacarias.snizer@gmail.com>
2020-07-19 16:43:40 +0800

    Enable CORS request

--- src/routes.php
+++ src/routes.php
@@ -7,6 +7,18 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
+    $app->add(function ($req, $res, $next) {
+        $response = $next($req, $res);
+        return $response
+                ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8082')
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
+
     $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
@@ -37,11 +49,20 @@ return function (App $app) {
         });
 
         $app->group('/section', function () use ($app) {
+            $app->get('/get/all', 'SectionController:getAll');
+
             $app->post('/create', 'SectionController:create');
         });
     });
 
     $app->get('/system-info', function (Request $request, Response $response) {
         return phpinfo();
+    });
+
+    // Catch-all route to serve a 404 Not Found page if none of the routes match
+    // NOTE: make sure this route is defined last
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+        $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+        return $handler($req, $res);
     });
 };
